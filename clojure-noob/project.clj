169dbc7edc1f68ba1b55cb1cb416a36685b00bd4
j0zeph <notaj@tutanota.de>
2022-10-22 08:04:57 +0300

    Fix lein repl not starting properly
    Double quotes in the project.clj are now escaped properly.

--- clojure-noob/project.clj
+++ clojure-noob/project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-noob "0.1.0-SNAPSHOT"
-  :description "A chronological work-through of the book "Clojure for the brave and true" by Daniel Higginbotham"
+  :description "A chronological work-through of the book \"Clojure for the brave and true\" by Daniel Higginbotham"
   :url "https://github.com/j0zeph/clojure-bat"
   :license {:name "MIT License"
             :url "https://mit-license.org/"}
