e0ed9931ac97a6a200b64f644f5f8ad0b9f53092
j0zeph <notaj@tutanota.de>
2022-10-24 14:08:38 +0300

    Change code to use examples not from the book.
    The code was changed to use made-up examples not from the book.
    This was done to allow me to think about the examples more deeply
    (which I think will not happen as well as it should, if I just type the
    examples in the book)

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,138 +4,169 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I am an enormously huge teapot!"))
+  (println "I am an enormously huge planet, Jupiter said!"))
+
+
+
+
 
 ;;pg41
-(defn error-message
-  [severity]
-  (str "OH GOD! IT'S A DISASTER! WE'RE "
-       (if (= severity :mild)
-         "MILDLY INCONVENIENCED!"
-         "DOOOOOOOMED!")))
+(defn danger-alert
+  [danger-level]
+  (str "That's terrifying! "
+       (if (= danger-level :not-acute)
+         "but also nothing that serious!"
+         "we may not make it alive!")))
 
-(error-message :mild)
-
-
-
-;;---------------------- DATA STRUCTURES
-
-;;numbers
-
-93
-1.2
-1/5
+(danger-alert :not-acute)
 
 
 
 
 
-;;---------------------- STRINGS
 
-"Lord Voldermot"
-"\"He who must not be named\""
-"\"Great cow of Moscow!\" - Hermes Conrad"
+
+
+
+
+;;#################### DATA STRUCTURES ####################
+
+
+;;---------- NUMBERS ----------
+
+
+99
+4.2
+22/7
+
+
+
+
+
+;;---------- STRINGS ----------
+
+
+"Clara Oswald"
+"\"The impossible girl\""
+"\"Hello Sweetie!\" - Puffy hair"
+
 
 ;;clojure only allows string concatenation via the `str` function
-(def character_name "Chewbacca")
-(str "\"Uggllglglglglglglglll\" -" character_name)
+(def mood "quite functional at the moment")
+(str "I feel " mood "at the moment, actually.")
 
 
 
 
 
-;;---------------------- MAPS (SIMILAR TO DICTIONARIES)
+;;---------- MAPS (similar to dictionaries) ----------
+
 
 ;;an empty map
 {}
 
-{:first-name "Charlie"
- :last-name "McFishwich"}
 
-;;associating "string-key" with the + function
-{"string-key" +}
+{:shirt-color "red"
+ :shirt-size "small"}
+
+
+;;associating the string "add-these" with the + function
+{"add-these" +}
+
 
 ;;maps can be nested
-{:name {:first "John" :middle "Jacob" :last "Jingleheimerschmidt"}}
+{:name {:first-day "Monday"
+        :middle-day "Wednesday"
+        :last-day "Sunday"}}
 
-;;You can use the hash-map function to create a map
+
+;;you can use the hash-map function to create a map
 (hash-map :a 1 :b 2)
 
 
-;;Look up values in a map with the `get` function
+;;look up values in a map with the `get` function
+(def letter-ascii (hash-map :a 97 :b 98 :c 99))
 
-;;example
-(def letter-map (hash-map :a 10 :b 20 :c 30))
+(get letter-ascii :a)
 
-(get letter-map :a)
 
-;;example
-(def nested-map (hash-map :a 0 :b (hash-map :c "ho hum")))
+(def nested-map (hash-map :a 97 :b (hash-map :c 99)))
 
 (get nested-map :b)
-
 
 
 ;;`get` will return `nil` if your key is not found
 (get {:u "here" :v "there"} :a)
 
+
 ;;you can also provide a default value for missing keys
-(def missing-keys (hash-map :r 45 :s 56 :t 78))
+(def get-more-ascii (hash-map :r 114 :s 115 :t 116))
 
-(get missing-keys :q "no key of that nature")
-
-
+(get get-more-ascii :q "no key of that nature")
 
 
 
-;;---------------------- KEYWORDS
 
 
-;;--> They are primarily used as keys in maps
+;;---------- VECTORS (similar to arrays in other languages) ----------
+;;
+;; they allow for mixed data
 
-:a
-:rumplestiltsken
-:34
+
+;;creating a simple vector literally
+[10 9 8 7 6 5 4 3 2 1]
+
+
+;;creating a vector using the `vector` function
+(vector "and a one" "and a two" "and a one, two, three, four")
+
+
+;;getting the zeroth element with `get`
+(get [10 9 8 7 6 5 4 3 2 1] 0)
+
+
+;;getting the first element
+(get ["x" {:name "Shayala Buff"} "brandishing a kinfe..."] 1)
+
+
+;;adding more elements to the vector using `conj`
+
+(conj [:z :y :x :w :v] :u) ;;gives  an output of [:z :y :x :w :v :u]
+
+
+
+
+
+;;---------- KEYWORDS (similar to arrays in other languages) ----------
+
+;;keywords are primarily used as keys in maps
+;;keywords start with ':'
+
+:z
+:space
+:43
 :_?
 
 
-;;--> Used as functions to look up a value in data structures
-
-(:a {:a 1 :b 2 :c 3}) ;;using :a to look that key up in the map
-
-(:d {:a 1 :b 2 :c 3} "No gnome know homes like Noah knows") ;;you can provide a default value as well.
+;;Keywords are also used as functions to look up a value in data structures
+;;using :a to look up the value of that key in a map
+(:a {:a 1 :b 2 :c 3})
 
 
-
-
-
-;;---------------------- VECTORS (SIMILAR TO ARRAYS)
-
-;;similar to arrays in other languages, and allows for mixed types
-
-
-[3 2 1] ;;a simple vector
-
-(get [3 2 1] 0) ;;getting the zeroth element
-
-(get ["a" {:name "Pugsley Winterbottom"} "c"] 1) ;;getting the first element
-
-(vector "creepy" "full" "moon") ;;using the `vector` function to create a vector
-
-
-;;Adding more elements to the vector using `conj`
-
-(conj [1 2 3 4] 5) ;;gives [1 2 3 4 5]
+;;looking up the value of the key :d, and also providing a default value,
+;;if the value is not present.
+(:d {:a 1 :b 2 :c 3} "No gnome know homes like Noah knows")
 
 
 
 
 
-;;---------------------- LISTS (SIMILAR TO VECTORS, BUT A LITTLE DIFFERENTS)
+;;---------- LISTS (similar to vectors, but different in some respects) ----------
 
-;;creating a list
 
-'(1 2 3 4) ;;how to construct a list literal . The quotation at the beginning is required
+;;creating a list literally.
+;;The single quote at the beginning is required
+'(1 2 3 4)
 
 
 ;;retrieving elements with the `nth` function (because you can't use get)
@@ -146,31 +177,40 @@
 
 
 ;;performance sidenote : using `nth` to get an element in a list is slower than using
-;;`get` to get an element from a vector. This is because `nth` has to visit every element in a list
+;;`get` to get an element from a vector. 
+;;This is because `nth` has to visit every element in a list
 
 
-;;lists can contain different values
+;;lists can contain different types of values (t is heterogenous)
 (list 1 "two" {3 4})
 
-;;elements are added to the beginning of the list
+
+;;using `conj` to add elements at the `beginning` of the list
 (conj '(1 2 3) 4)
 
 
 
 
 
-;;---------------------- SETS (NO DUPLICATES ALLOWED)
-
+;;---------- SETS (no duplicates allowed) ----------
+;;
 ;;Collections of unique values
 
 
-;;How to make a literal set
+;;creating a set, literally
 #{"kurt vonnegut" 20 :icicle}
 
 
-;;You may also use `hash-set` to create a set
-;;Note that duplicates are removed
+;;Using the `hash-set` function to create a set (Note that duplicates will be removed)
 (hash-set 1 1 2 2)
+
+
+;;making a set out of a vector using the `set `function
+(set [1 2 3 3 3 8 8 9])
+
+
+;;making a set out of a map using the `set `function
+(set {:d "here" :r "there"})
 
 
 ;;using conj adds an element (if it doesn't exist yet) to the set.
@@ -179,26 +219,20 @@
 (conj #{:q :r :s} :r :a)
 
 
-;;you can also make a set out of a vector and a list using the `set` function
-
-(set [1 2 3 3 3 8 8 9])
-
-(set {:d "here" :r "there"})
-
-
-;;you can check whether an element exists in a set using the `contains?` function
+;;check whether an element exists in a set using the `contains?` function
 
 (def our_set (set ["a" 1 :c]))
 
 (contains? our_set :c)
 
 
-;;you can check for an existing element using a keyword as well ...
+;;you can also check for an existing element using a keyword as well
+;;`nil` will be returned if the element does not exist
 
 (:a #{:a :b})
 
 
-;;you can also use the `get` function as well...
+;;you can also use the `get` function to check if an element exists
 
 (get (hash-set :a :d :r "here" "there" "everywhere") :r)
 
@@ -214,45 +248,59 @@
 ;;###################### FUNCTIONS  ######################
 
 
+;;---------- CALLING/USING FUNCTIONS ----------
 
-;;---------------------- CALLING FUNCTIONS
 
-(+ 1 2 3 4)
+(+ 5 5 5)
 
-(* 1 2 3 4)
 
-(first [1 2 3 4])
+(* 5 4 3 2 1)
 
-(last [1 2 3 4])
 
-;;recall that `or` returns the first truthy value (or if none exist, the last value)
-;;the function below will return
+(first [1 2 3 4 5 6 7 8])
+
+
+(last ["a one" "a two" "a three" "four"])
+
+
+;;recall that `or` returns the first truthy value (or if none exists, the last value)
 
 (or + -)
 
 
-;;you can therefore use the return value of one function as a function for some other operation
+;;you can therefore use the return value of one function as a function that does something else
 
 ((or + -) 1 2)
 
+
+;;recall that `and` returns the first falsey value (or if none exists, the last value)
+
 ((and (= 1 1) +) 1 2 3)
+
 
 ((first [+ 0]) 1 2 3)
 
 
-;;Higher order functions are those that can either take a function as an argument
-;;for example the `map` function (that applies another function over every element in a collection)
+;;higher-order functions are those that can either take a function as an argument or
+;;return a function
 
-(inc 1.1)
+;;for example the `map` function (that applies a function over every element in a collection)
 
-(map inc [1 2 3 4]) ;;Note: `map` does not return a vector, even when we gave it one
+(inc 1.1) ;; recall that the `inc` function increases whatever value we give it
+
+
+(map inc [1 2 3 4])
 
 
 
-;;---------------------- special forms
 
+
+;;---------- SPECIAL FORMS ----------
+;;
 ;;do not always evaluate all of their operands.
-;;An if statement is an example
+
+
+;;An if statement is an example of a special form
 
 (def using-twitter true)
 
@@ -267,43 +315,55 @@
   (println "You are not using Twitter at the moment"))
 
 
+;; special forms also CANNOT be used as arguments to other functions
 
-;;---------------------- DEFINING FUNCTIONS
 
 
-;;general structure of function declaration ----------------------
+;;---------- DEFINING FUNCTIONS ----------
+
+
+;;general structure of function declaration
 
 ;(defn <functionname>
 ;;<optional function description / docstring>
 ;;<[arguments]>
 ;;<function body>)
 
-(defn too-enthusiastic
-  "Return a cheer that might be too enthusiastic"
-  [name]
-  (str "OH. MY. GOD! " name " YOU ARE MOST DEFINETLY LIKE THE BEST "
-       "MAN SLASH WOMAN EVER I LOVE YOU AND WE SHOULD RUN AWAY SOMEWHERE"))
 
-(too-enthusiastic "Zelda")
+(defn hello-friend
+  "Tell the friend about some secret"
+  [friend-name]
+  (str "Hello " friend-name " , \nWhat I'm about to tell you is top-secret. "
+       "\nA conspiracy bigger than all of us ..."))
+
+(println (hello-friend "Taylor"))
 
 
-;;functions can have different number of parameters a.k.a arity ----------------------
+;;functions can have any number of parameters (they can have any "arity")
 
 (defn no-params
   "I take no parameters!"
   [])
 
 (defn one-param
+  "Functions that takes one parameter"
   [x]
   (str "I take one parameter: " x))
 
 (defn two-params
+  "Function that takes two parameters"
   [x y]
-  (str "Two parameters! That's nothing! Pah! I will smooth them "
-       "together to spite you! " x y))
+  (println (str "I take two parameters, and they are: " x " and " y " too.")))
 
 
-;;functions can have multiple arities, to decide which version of the function runs ----------------------
+(no-params)
+
+(one-param "this one")
+
+(two-params "\"this one\"" "\"this one\"")
+
+
+;;functions can have multiple arities, to decide which version of the function runs
 ;;this is a nice way to provide default arguments to your function
 
 (defn name-and-location
@@ -326,92 +386,98 @@
 (name-and-location)
 
 
-;;rest parameter & ----------------------
 
-(defn codger-communication
-  [whippersnapper]
-  (str "Get off my lawn, " whippersnapper "!!!"))
+;;---------- REST PARAMETER (&) ----------
+;;
+;; Allows  you to group provided parameters into a list
+;; When mixing normal parameters and rest parameters, the rest parameter must always come last
+
+(defn hello-to-you
+  [person-name]
+  (str "Hello, " person-name "!"))
 
 
-(defn codger
-  [& whippersnapper]
-  (map codger-communication whippersnapper))
+(defn hello-to-your-friends
+  [& your-friends]
+  (map hello-to-you your-friends))
 
-(codger "Billy" "Anne-Marie" "The incredible Bulk")
+(hello-to-your-friends "Magdalena", "Rosetta", "Clarice", "Rene")
 
 
 ;;mixing rest-parameters and normal parameters ----------------------
-;;The rest parameter must come last if you are doing this
 
-(defn favorite-things
-  [person-name & things]
-  (str "Hi, " person-name ", here are my favorite things: "
-       (clojure.string/join ", " things)))
+(defn pick-combat-items
+  [character-name & combat-items]
+  (str "Hi " character-name ", here are your combat items; "
+       (clojure.string/join ", " combat-items)))
 
-(favorite-things "Doreen" "gum" "shoes" "kara-te")
-
-
-
-;;DESTRUCTURING ----------------------
-
-
-;;Return the first element of a collection ----------------------
-
-(defn my-first
-  [[first-thing]]
-  (str "The first thing is: " first-thing))
-
-(my-first ["oven" "bike" "war-axe"])
-
-
-(defn chooser
-  "Lists the first choice, second choice, and the rest of the choices"
-  [[first-choice second-choice & unimportant-choices]]
-  (println (str "Your first choice is: " first-choice))
-  (println (str "Your second choice is: " second-choice))
-  (println (str "We're ignoring the rest of your choices. "
-                "Here they are in case you need to cry over them: "
-                (clojure.string/join ", " unimportant-choices))))
-
-(chooser ["Marmalade" "Handsome Jack" "Pigpen" "Aquaman"])
+(pick-combat-items "Stranger" "spoon" "pen" "sword")
 
 
 
-;;Destructure maps ----------------------
-
-(defn announce-treasure-location
-  "Shows the latitude and longitude of the treasure"
-  [{latitude :lat longitude :lng}]
-  (println (str "Treasure is at latitude: " latitude))
-  (println (str "Treasure is at longitude: " longitude)))
-
-(announce-treasure-location {:lat 28.2 :lng 81.33})
+;;---------- DESTRUCTURING ----------
+;;
+;;Allows you to concisely bind names to values within a collection
 
 
+;;Returning the first element of a collection (a vector in this case) using destructuring
 
-;;Destructure maps, simpler version ----------------------
+(defn first-place
+  [[first-runner]]
+  (str "The runner in first place is: " first-runner))
 
-(defn announce-treasure-location-simpler
-  "Shows the latitude and longitude of the treasure"
-  [{:keys [lat lng]}]
-  (println (str "Treasure is at latitude: " lat))
-  (println (str "Treasure is at longitude: " lng))
-)
+(first-place ["Julien" "Borris" "Travis"])
+
+
+(defn give-medals
+  "Gives the first three runners a gold, silver, and bronze medal, then lists everyone else"
+  [[first-runner second-runner third-runner & everyone-else]]
+  (println (str "The gold medal goes to: " first-runner))
+  (println (str "The silver medal goes to: " second-runner))
+  (println (str "The bronze medal goes to: " third-runner))
+  (println (str "And here is everyone else: " (clojure.string/join ", " everyone-else))))
+
+(give-medals  ["Julien" "Borris" "Travis" "Marcus" "Daniel" "Freddie"])
+
+
+
+;;Destructuring maps
+
+(defn announce-point-location
+  "Shows the x, y, and z coordinate location of a point"
+  [{x-coordinate :x y-coordinate :y z-coordinate :z}]
+  (println (str "x-coordinate is: " x-coordinate))
+  (println (str "y-coordinate is: " y-coordinate))
+  (println (str "z-coordinate is: " z-coordinate)))
+
+(announce-point-location {:x 200 :y 300 :z 120})
+
+
+
+;;Destructuring maps, simpler version using `:keys` to more directly extract the keywords
+;;from a map
+
+(defn announce-point-location-simpler
+  "Shows the x, y, and z coordinate location of a point"
+  [{:keys [x y z]}]
+  (println (str "x-coordinate is: " x))
+  (println (str "y-coordinate is: " y))
+  (println (str "z-coordinate is: " z)))
 
 ;; note: you have to use the same names as the actual keys in the map
 
-(announce-treasure-location-simpler {:lat 30 :lng 40})
+(announce-point-location-simpler {:x 200 :y 300 :z 120})
 
 
 
-;;Destructure maps, while retaining the original map ----------------------
+;;Destructure maps, while retaining the original map using the :as keyword
 
-(defn receive-treasure-location
-  [{:keys [lat lng] :as treasure-location}]
-  (println (str "Treasure is at latitude: " lat))
-  (println (str "Treasure is at longitude: " lng))
+(defn receive-point-location
+  [{:keys [x y z] :as point-location}]
+  (println (str "x-coordinate is: " x))
+  (println (str "y-coordinate is: " y))
+  (println (str "z-coordinate is: " z))
+  
+  (println (str "The original point location was: " point-location)))
 
-  (println (str "Received locations: " treasure-location))
-)
-
-(receive-treasure-location {:lat 57.981224 :lng 7.045005})
+(receive-point-location {:x 10 :y 20 :z 30})
